1c128da04893bb837d34116670eed8b083b75a6a
chunk offset <chunkoffset@gmail.com>
2015-02-04 00:37:59 +0900

    小書き文字、アルファベット、継続　に対応

--- be_unko.rb
+++ be_unko.rb
@@ -2,25 +2,23 @@
 require 'ostruct'
 
 module BeUnko
-  # clear continues
-  def self.clear_continues(continues, except)
-    continues.each_key{|key|
-      if except != key.to_s then 
-        continues[key] = 0
-      end
-    }
+  #-------------------
+  # Utility
+  #-------------------
+  def self.max_cyclic(org, max)
+    if org <= max then
+      return org
+    else
+      return 0
+    end
   end
-  # increment continues (cyclic)
-  def self.increment_continues(continues, target, max)
-    continues.each{|key, value|
-      if target == key.to_s then
-        value = value < max ? value+1 : 0
-        continues[key] = value
-      end
-    }
+  def self.max_stop(org, max)
+    if org <= max then
+      return org
+    else
+      return max
+    end
   end
-  # Patterns
-
   #-------------------
   # module_function(s)
   #-------------------
@@ -29,32 +27,65 @@ module BeUnko
   # convert string
   #
   def string(org)
-    # continues counter
-    continues = {hira:0, kata:0, alpha:0}
-    # Convert
+    serial = 0;
+    last_type = '-';
     result = ""
+    # convert
     org.split(//).each{|ch|
       # ひらがな
       if    /\p{Hiragana}/ =~ ch then
-        replace = ['う','ん','こ']
-        ch = replace[continues[:hira]]
-
-        continues = increment_continues(continues, 'hira', 3)
-        continues = clear_continues(continues, 'hira')
+        # 小書き文字
+        if /[ぁぃぅぇぉっゃゅょゎ]/ =~ ch then
+          replace = ['ぅ','っ','ぉ']
+          serial = ('hira'==last_type) ? serial : 0;
+        # ナミ字
+        else
+          replace = ['う','ん','こ']
+          serial = ('hira'==last_type) ? max_cyclic(serial+1,replace.length-1) : 0;
+        end
+        ch = replace[serial]
+        last_type = 'hira'
       # カタカナ
       elsif /\p{Katakana}/ =~ ch then
-        replace = ['ウ','ン','コ']
-        ch = replace[continues[:kata]]
-        
-        continues = increment_continues(continues, 'kata', 3)
-        continues = clear_continues(continues, 'kata')
+        # 小書き文字
+        if /[ァィゥェォヵヶッャュョヮ]/ =~ ch then
+          replace = ['ゥ','ッ','ォ']
+          serial = ('kata'==last_type) ? serial : 0;
+        # ナミ字(半角）
+        elsif /[ｱ-ﾝ]/ =~ ch then
+          replace = ['ｳ','ﾝ','ｺ']
+          serial = ('kata'==last_type) ? max_cyclic(serial+1,replace.length-1) : 0;
+        # ナミ字
+        else
+          replace = ['ウ','ン','コ']
+          serial = ('kata'==last_type) ? max_cyclic(serial+1,replace.length-1) : 0;
+        end
+        ch = replace[serial]
+        last_type = 'kata'
       # 漢字
       elsif /[一-龠々]/ =~ ch then
         ch = '糞'
-        continues = clear_continues(continues, nil)
+        last_type = 'kanji'
+      # アルファベット
+      elsif /[a-zA-Z]/ =~ ch then
+        # 小文字
+        if /[a-z]/ =~ ch then
+          replace = ['u','n','k','o']
+        # 大文字
+        else
+          replace = ['U','N','K','O']
+        end
+        serial = ('alpha'==last_type) ? max_stop(serial+1,replace.length-1) : 0;
+        ch = replace[serial]
+        last_type = 'alpha'
+      # 継続
+      elsif /[ー―～・]/ =~ ch then
+        # last_type更新しない
+      # その他
       else
-        continues = clear_continues(continues, nil)
+        last_type = '-'
       end
+      
       result = result + ch.to_s
     }
     
--- sample.rb
+++ sample.rb
@@ -2,7 +2,26 @@
 
 require "./be_unko.rb"
 
-org ="これが元の文章です。これはサンプルです。（括弧などの記号は変換されません）"
+org ="これが元の文章です。これはサンプルです。（記号や数値は変換されません）"
 puts org
+puts "↓"
 puts BeUnko::string(org)
+puts ""
 
+org ="ﾊﾝｶｸｶﾅ、Alphabetにも対応しています。"
+puts org
+puts "↓"
+puts BeUnko::string(org)
+puts ""
+
+org ="スーパーマケーットに行きましょう。"
+puts org
+puts "↓"
+puts BeUnko::string(org)
+puts ""
+
+org ="コーギー犬"
+puts org
+puts "↓"
+puts BeUnko::string(org)
+puts ""
